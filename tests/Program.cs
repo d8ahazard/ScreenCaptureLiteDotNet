bf9f5fef4ce3508e8ef3afa5a57655a75515315a
d8ahazard <d8ahazard@gmail.com>
2020-06-03 15:03:57 -0500

    Pretty and clean is good.

---
+++ tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ScreenCaptureLiteDotNet;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var sc = ScreenCapture.GetMonitors();
+            Console.WriteLine("Monitors got!");
+        }
+    }
+}
